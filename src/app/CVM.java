715d6f89ac31041564eb4f0359ad81589e634bf1
Nassim <nassim.et@hotmail.fr>
2019-12-14 14:07:04 +0100

    execution plus longue

--- src/app/CVM.java
+++ src/app/CVM.java
@@ -33,7 +33,7 @@ public class CVM extends AbstractCVM {
 	public static void main(String[] args) {
 		try {
 			CVM c = new CVM();
-			c.startStandardLifeCycle(20000L);
+			c.startStandardLifeCycle(40000L);
 			Thread.sleep(5000L);
 			System.exit(0);
 		} catch (Exception e) { throw new RuntimeException(e); }
